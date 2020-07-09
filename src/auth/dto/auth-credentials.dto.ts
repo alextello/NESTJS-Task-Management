77889a370c67f94a25e6df65da500002e27b45a1
unknown <alextello1@hotmail.com>
2020-07-09 11:14:48 -0600

    Manejo de usuarios duplicados

--- src/auth/dto/auth-credentials.dto.ts
+++ src/auth/dto/auth-credentials.dto.ts
@@ -1,4 +1,15 @@
+import { IsString, MaxLength, MinLength } from 'class-validator';
+
 export class AuthCredentialsDto {
+  @IsString({ message: 'Formato de usuario no válido' })
+  @MinLength(5, {
+    message: 'El usuario debe ser de al menos 5 caracteres',
+  })
+  @MaxLength(20, {
+    message: 'El usuario no debe exceder los 20 caracteres',
+  })
   username: string;
+  @IsString({ message: 'Formato de contraseña no válido' })
+  @MinLength(8, { message: 'La contraseña debe ser de al menos 8 caracteres' })
   password: string;
 }
