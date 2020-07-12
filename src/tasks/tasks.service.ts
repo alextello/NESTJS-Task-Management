fdaeee06bd0b7110f4ba882c8db530cef7e338c7
unknown <alextello1@hotmail.com>
2020-07-12 13:24:24 -0600

    actualizando y eliminando tareas bajo autorización

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -17,8 +17,10 @@ export class TasksService {
     return await this.taskRepository.getTasks(filterDto, user);
   }
 
-  async getTaskById(id: number): Promise<Task> {
-    const found = await this.taskRepository.findOne(id);
+  async getTaskById(id: number, user: User): Promise<Task> {
+    const found = await this.taskRepository.findOne({
+      where: { id, userId: user.id },
+    });
     if (!found) {
       throw new NotFoundException(`La tarea con el id ${id} no existe`);
     }
@@ -29,13 +31,17 @@ export class TasksService {
     return this.taskRepository.createTask(createTaskDto, user);
   }
 
-  async deleteTask(id: number): Promise<Task> {
-    const task = await this.getTaskById(id);
+  async deleteTask(id: number, user: User): Promise<Task> {
+    const task = await this.getTaskById(id, user);
     return (await task).remove();
   }
 
-  async updateTaskStatus(id: number, status: TaskStatus): Promise<Task> {
-    const task = await this.getTaskById(id);
+  async updateTaskStatus(
+    id: number,
+    status: TaskStatus,
+    user: User,
+  ): Promise<Task> {
+    const task = await this.getTaskById(id, user);
     task.status = status;
     await task.save();
     return task;
